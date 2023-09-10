408d1ba95e373ca816462f1559c0b599b032f761
Pascal Stepien <pstepien@hotmail.co.uk>
2023-09-10 17:04:41 +0100

    initial commit

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace lan_game_hub_api.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string UserRole { get; set; }
+    }
+}
