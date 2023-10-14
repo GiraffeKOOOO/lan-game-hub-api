ec668bb46036b916e9a4448805171d3dde81cf75
GiraffeKOOOO <pstepien@hotmail.co.uk>
2023-10-14 19:17:12 +0100

    feature: added game state and image string to the game model

--- Models/Game.cs
+++ Models/Game.cs
@@ -12,11 +12,17 @@ namespace lan_game_hub_api.Models
 
         [Column("game_name")]
         public string game_name { get; set; }
+        
+        [Column("game_state")]
+        public string game_state { get; set; }
 
         [Column("game_mode")]
         public string game_mode { get; set; }
 
         [Column("game_start_time")]
         public string game_start_time { get; set; }
+
+        [Column("game_image_string")]
+        public string game_image_string { get; set; }
     }
 }
